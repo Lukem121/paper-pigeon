9b9b5e192d7f4a9bc6683afa1d9081115632b13b
luke askew <lukeask@hotmail.co.uk>
2020-10-06 14:46:25 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# paper-pigeon
+# Installation
+```console
+npm install paper-pigeon --save
+```
+# Usage
+```js
+import Discord from './index.js';
+
+const send = new Discord("WEBHOOK_URL");
+send.message("Starting server!");
+send.success("Data loaded from API");
+send.error("Failed to load resource");
+```
+# Get Discord Webhook URL
+Open Discord -> Integrations -> Create Webhook -> Copy Webhook URL.
+
+# Demo
+![demo](https://user-images.githubusercontent.com/30378184/95209529-5c735e00-07e2-11eb-9724-e5a49b1e8585.jpg)
---
+++ index.js
@@ -0,0 +1,34 @@
+export default class Discord {
+    constructor(webhook) {
+      this.webhook = webhook;
+      this.xhr = new XMLHttpRequest();
+    }
+  
+    success(msg) {
+        this.xhr.open("POST", this.webhook, true);
+        this.xhr.setRequestHeader('Content-Type', 'application/json');
+        this.xhr.send(JSON.stringify({
+            content: msg,
+            username: "Pigeon: Success",
+            avatar_url:"https://user-images.githubusercontent.com/30378184/95204502-1a471e00-07dc-11eb-9f40-ec39076cce1f.jpg"
+        }));
+    };
+    error(msg) {
+        this.xhr.open("POST", this.webhook, true);
+        this.xhr.setRequestHeader('Content-Type', 'application/json');
+        this.xhr.send(JSON.stringify({
+            content: msg,
+            username: "Pigeon: Error",
+            avatar_url:"https://user-images.githubusercontent.com/30378184/95203862-3a2a1200-07db-11eb-9992-605c2a8c75d5.jpg"
+        }));
+    };
+    message(msg) {
+        this.xhr.open("POST", this.webhook, true);
+        this.xhr.setRequestHeader('Content-Type', 'application/json');
+        this.xhr.send(JSON.stringify({
+            content: msg,
+            username: "Pigeon",
+            avatar_url:"https://user-images.githubusercontent.com/30378184/95204787-7742d400-07dc-11eb-9271-2e5a92a34fcc.jpg"
+        }));
+    };
+}
